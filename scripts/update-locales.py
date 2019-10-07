d78ffc3cbd42f705e53e0f062ce0b5ecb9227a29
fpemud <fpemud@sina.com>
2019-10-07 15:49:57 +0800

    fix

--- scripts/update-locales.py
+++ scripts/update-locales.py
@@ -3,7 +3,7 @@
 
 """
     This file is part of Torrent Search.
-    
+
     Torrent Search is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
