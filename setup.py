797ca1a6aea64057d4c7046a6fd8d0a9e82020e5
fpemud <fpemud@sina.com>
2019-10-07 13:30:52 +0800

    fix

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 """
     This file is part of Torrent Search.
-    
+
     Torrent Search is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
@@ -17,51 +17,57 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
+import os
+import sys
 from distutils.core import setup
-import os, sys
 
-def walk_lib_files(res,path,files):
-   if ".svn" in path:
-      return
-   l=[]
-   for i in files:
-      if os.path.isfile(os.path.join(path,i)):
-         if i[-1]!='~' and i[-4:]!='.bak':
-            l.append(os.path.join(path,i))
-   if l:
-      res.append((os.path.join("lib",path[4:]),l))
+
+def _walk_lib_files(res, path, files):
+    if ".git" in path:
+        return
+    tl = []
+    for i in files:
+        if os.path.isfile(os.path.join(path, i)):
+            if i[-1] != "~" and i[-4:] != ".bak":
+                tl.append(os.path.join(path, i))
+    if tl != []:
+        res.append((os.path.join("lib", path[4:]), l))
+
 
 def list_lib_files():
-   res=[]
-   os.path.walk('lib',walk_lib_files,res)
-   return res
+    res = []
+    os.path.walk("lib", _walk_lib_files, res)
+    return res
 
-def walk_share_files(res,path,files):
-   if ".svn" in path:
-      return
-   l=[]
-   for i in files:
-      if os.path.isfile(os.path.join(path,i)):
-         if i[-1]!='~' and i[-4:]!='.bak':
-            l.append(os.path.join(path,i))
-   if l:
-      res.append((os.path.join("share",path[6:]),l))
+
+def _walk_share_files(res, path, files):
+    if ".git" in path:
+        return
+    tl = []
+    for i in files:
+        if os.path.isfile(os.path.join(path, i)):
+            if i[-1] != "~" and i[-4:] != ".bak":
+                tl.append(os.path.join(path, i))
+    if tl != []:
+        res.append((os.path.join("share", path[6:]), l))
+
 
 def list_share_files():
-   res=[]
-   os.path.walk('share',walk_share_files,res)
-   return res
+    res = []
+    os.path.walk("share", _walk_share_files, res)
+    return res
 
-setup(name='torrent-search',
-   version="0.11.2",
-   author='Gwendal Le Bihan',
-   author_email='gwendal.lebihan.dev@gmail.com',
-   maintainer='Gwendal Le Bihan',
-   maintainer_email='gwendal.lebihan.dev@gmail.com',
-   description='Search for torrents on different websites',
-   scripts=["torrent-search", "torrent-search-gnomeapplet"],
-   packages=["TorrentSearch","TorrentSearch.exceptions"],
-   data_files=list_share_files()+list_lib_files(),
-   url="http://torrent-search.sourceforge.net",
-   download_url="http://sourceforge.net/projects/torrent-search/files/",
-   )
+
+setup(name="torrent-search",
+      version="0.11.2",
+      author="Gwendal Le Bihan",
+      author_email="gwendal.lebihan.dev@gmail.com",
+      maintainer="Gwendal Le Bihan",
+      maintainer_email="gwendal.lebihan.dev@gmail.com",
+      description="Search for torrents on different websites",
+      scripts=["torrent-search", "torrent-search-gnomeapplet"],
+      packages=["TorrentSearch", "TorrentSearch.exceptions"],
+      data_files=list_share_files()+list_lib_files(),
+      url="http://torrent-search.sourceforge.net",
+      download_url="http://sourceforge.net/projects/torrent-search/files/",
+      )
